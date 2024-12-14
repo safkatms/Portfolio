2c4f5cff8c96dbdd05460979e9742b15376cca4e
safkatms <safkatmahmudsakib@gmail.com>
2024-12-14 13:18:28 +0600

    header

--- portfolio-website/src/app/page.tsx
+++ portfolio-website/src/app/page.tsx
@@ -4,7 +4,14 @@ export default function Home() {
   return (
     <>
       <Header />
-      
+      <section className="flex justify-between h-max-100% w-max-100%">
+        <div className="">
+          
+        </div>
+        <div>
+
+        </div>
+      </section>
     </>
   );
 }
